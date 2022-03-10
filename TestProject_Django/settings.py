66a140f6fb608aa8a11e5a420de698594fb4e533
dchattop <91043054+dchattop@users.noreply.github.com>
2022-03-10 12:17:06 +0530

    Update settings.py

--- TestProject_Django/settings.py
+++ TestProject_Django/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from socket import gethostname
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +30,10 @@ SECRET_KEY = 'django-insecure-8rj6om469z183$x#6ey=08#e%*q4t@!i9v(3s2$f@m*j#(lj4r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://test-project-django-git-rest-api-poc.apps.ssa-prod.20r1.p1.openshiftapps.com/']
+ALLOWED_HOSTS = [gethostname(), 
+
+    os.environ.get('OPENSHIFT_APP_DNS'),
+                ]
 
 
 # Application definition
